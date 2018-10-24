e531b70f84f98026b8708b485ee37d9fae4a1c32
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2018-10-24 21:54:06 +0800

    Initial Version

---
+++ src/main/java/sg/petsworld/sgpetstore/SgPetstoreApplication.java
@@ -0,0 +1,12 @@
+package sg.petsworld.sgpetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgPetstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgPetstoreApplication.class, args);
+    }
+}
