07a29bcc109f02ef5e365231742c87c7dd1509ca
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2018-11-18 06:46:54 +0800

    refactoring to multi module maven project.There are some issues with Maven plugins. closes #3

--- pet-clinic-web/src/test/java/sgpetstore/SgPetstoreApplicationTests.java
+++ pet-clinic-web/src/test/java/sgpetstore/SgPetstoreApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import sg.petsworld.sgpetstore.SgPetstoreApplication;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SgPetstoreApplication.class)
 public class SgPetstoreApplicationTests {
 
     @Test
