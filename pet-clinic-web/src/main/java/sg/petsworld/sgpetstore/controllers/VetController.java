12230db62e2021d7051e8f2f23268f92a55c8a2a
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2018-11-22 21:10:49 +0800

    Loaded all vets on Owners index page. Closes #15

--- pet-clinic-web/src/main/java/sg/petsworld/sgpetstore/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sg/petsworld/sgpetstore/controllers/VetController.java
@@ -1,7 +1,9 @@
 package sg.petsworld.sgpetstore.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sg.petsworld.sgpetstore.services.VetService;
 
 /*
  * Created by NR or 19 Nov 2018
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","vets/index","vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
