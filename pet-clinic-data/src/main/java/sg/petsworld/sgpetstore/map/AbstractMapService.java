4b965e671ac894ad9f856bd1d048c9a2d60effb3
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2018-11-22 21:57:21 +0800

    Refactored MapImpl ro abstract id generation. Closes #23

--- pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/map/AbstractMapService.java
@@ -1,17 +1,16 @@
 package sg.petsworld.sgpetstore.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import sg.petsworld.sgpetstore.model.BaseEntity;
+
+import java.util.*;
 
 /*
  * Created by NR on 18 Nov 2018
  */
 
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap();
+    protected Map<Long, T> map = new HashMap();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -22,8 +21,17 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+
+        if(object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
+
         return object;
     }
 
@@ -33,5 +41,16 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry-> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+
+        Long nextId = null;
+        try {
+            nextId=Collections.max(map.keySet())+1;
+        } catch(NoSuchElementException e) {
+            nextId=1L;
+        }
+        return nextId;
     }
 }
