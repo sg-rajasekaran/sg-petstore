f24a72a95a94f98e2c9288252d9e2943f6d976ca
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2018-12-02 13:12:19 +0800

    Added PetType Service. Closes #29

---
+++ pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package sg.petsworld.sgpetstore.map;
+
+import org.springframework.stereotype.Service;
+import sg.petsworld.sgpetstore.model.PetType;
+import sg.petsworld.sgpetstore.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
---
+++ pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package sg.petsworld.sgpetstore.services;
+
+import sg.petsworld.sgpetstore.model.PetType;
+
+/*
+ * created by NR on 2 Dec 2018
+ */
+public interface PetTypeService extends CrudService<PetType,Long> {
+
+}
