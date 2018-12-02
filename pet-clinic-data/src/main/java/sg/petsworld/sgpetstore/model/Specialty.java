6db4e50628f3f60004b3c7719b020d94f79a603b
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2018-12-02 09:35:29 +0800

    Created Vet Specialties, associated to Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/model/Specialty.java
@@ -0,0 +1,16 @@
+package sg.petsworld.sgpetstore.model;
+
+/*
+ * Created by NR 2 Dec 2018
+ */
+public class Specialty extends BaseEntity {
+    public String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/model/Vet.java
+++ pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/model/Vet.java
@@ -1,4 +1,13 @@
 package sg.petsworld.sgpetstore.model;
 
 public class Vet extends Person {
+    private Set<Specialty> specialities;
+
+    public Set<Specialty> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Specialty> specialities) {
+        this.specialities = specialities;
+    }
 }
