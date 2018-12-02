e839427a36ed09636776b6cf7d56a32f3cf880fb
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2018-12-02 09:19:15 +0800

    Added Visits, Updated Owner with Pet relationship. Closes #26

--- pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/model/Owner.java
+++ pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/model/Owner.java
@@ -1,4 +1,14 @@
 package sg.petsworld.sgpetstore.model;
 
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/model/Visit.java
@@ -0,0 +1,37 @@
+package sg.petsworld.sgpetstore.model;
+
+import java.time.LocalDate;
+
+/*
+ * created by NR on 2 Dec 2018
+ */
+public class Visit extends BaseEntity{
+
+    private LocalDate localDate;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
