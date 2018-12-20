d036bdf470d059c3660e9eac2e7cb65980f4efd5
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2018-12-20 21:30:03 +0800

    Added Pets and Contact info. Closes #33

--- pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/model/Pet.java
+++ pet-clinic-data/src/main/java/sg/petsworld/sgpetstore/model/Pet.java
@@ -4,10 +4,18 @@ import java.time.LocalDate;
 
 public class Pet extends BaseEntity{
 
+    private String name;
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     public PetType getPetType() {
         return petType;
     }
@@ -31,4 +39,6 @@ public class Pet extends BaseEntity{
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
+
+
 }
--- pet-clinic-web/src/main/java/sg/petsworld/sgpetstore/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/sg/petsworld/sgpetstore/bootstrap/DataLoader.java
@@ -6,11 +6,14 @@ package sg.petsworld.sgpetstore.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import sg.petsworld.sgpetstore.model.Owner;
+import sg.petsworld.sgpetstore.model.Pet;
 import sg.petsworld.sgpetstore.model.PetType;
 import sg.petsworld.sgpetstore.model.Vet;
 import sg.petsworld.sgpetstore.services.OwnerService;
 import sg.petsworld.sgpetstore.services.PetTypeService;
 import sg.petsworld.sgpetstore.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -39,11 +42,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("weston");
+        owner1.setAddress("123, Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1231231234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glananne");
+        owner2.setAddress("123, Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("1231231234");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
+
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners Data");
